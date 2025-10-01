5a674650807b06a62ae139637904c20363a2b388
pia-digiteer <pia.digiteer@gmail.com>
2025-10-01 11:13:56 +0800

    init new Rails app
    supports core tech needed: Stimulus, Slim, Bootstrap, SASS/SCSS, GSAP, Splide

---
+++ config/importmap.rb
@@ -0,0 +1,9 @@
+# Pin npm packages by running ./bin/importmap
+
+pin "application"
+pin "@hotwired/turbo-rails", to: "turbo.min.js"
+pin "@hotwired/stimulus", to: "stimulus.min.js"
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
+pin_all_from "app/javascript/controllers", under: "controllers"
+pin "gsap" # @3.13.0
+pin "@splidejs/splide", to: "@splidejs--splide.js" # @4.1.4
