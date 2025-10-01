ea15775cd31b8f0b8a5ad2184b52517460aee2b4
pia-digiteer <pia.digiteer@gmail.com>
2025-10-01 23:40:17 +0800

    Fixed rails test failure, read further in description
    caused by: uncited routes for 'site' module; previously removed attribute 'title' still mentioned in posts model instances for tests

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -17,7 +17,7 @@ class PostsControllerTest < ActionDispatch::IntegrationTest
 
   test "should create post" do
     assert_difference("Post.count") do
-      post posts_url, params: { post: { body: @post.body, likes: @post.likes, shares: @post.shares, title: @post.title } }
+      post posts_url, params: { post: { body: @post.body, likes: @post.likes, shares: @post.shares } }
     end
 
     assert_redirected_to post_url(Post.last)
@@ -34,7 +34,7 @@ class PostsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update post" do
-    patch post_url(@post), params: { post: { body: @post.body, likes: @post.likes, shares: @post.shares, title: @post.title } }
+    patch post_url(@post), params: { post: { body: @post.body, likes: @post.likes, shares: @post.shares } }
     assert_redirected_to post_url(@post)
   end
 
--- test/controllers/site/home_controller_test.rb
+++ test/controllers/site/home_controller_test.rb
@@ -1,8 +1,10 @@
 require "test_helper"
 
-class Site::HomeControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get site_home_index_url
-    assert_response :success
+module Site
+  class HomeControllerTest < ActionDispatch::IntegrationTest
+    test "should get index" do
+      get site_home_index_url
+      assert_response :success
+    end
   end
 end
